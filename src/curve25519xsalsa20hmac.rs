24ae9d0850208224867f9a8ad30488f512763506
maurges <nikita@dfns.co>
2024-10-14 11:45:52 +0200

    Add compat test for eddsa-xsalsa-hmac

--- src/curve25519xsalsa20hmac.rs
+++ src/curve25519xsalsa20hmac.rs
@@ -80,10 +80,42 @@ mod test {
         let pubkey_bytes =
             hex::decode("d24f3652e2100524d31ae794e781c4cd0b4f53e2bb02665b85f9c71d5e80ab69")
                 .unwrap();
+        // Would be a good idea to generate them on the fly with openssl, but it's
+        // not available in all distributions by default, for example on macos
 
         let pubkey = super::PublicKey::from_bytes(pubkey_bytes).unwrap();
         let key =
             super::PrivateKey::from_eddsa_pkey_bytes(key_bytes[0..32].try_into().unwrap()).unwrap();
         assert_eq!(key.public_key(), pubkey);
+    }
+
+    #[test]
+    fn compat() {
+        // Sadly we already use this in production to encrypt data for long-term
+        // storage, and unless something breaking is discovered, we need to be
+        // able to still decrypt the data we encrypted at the old revision of
+        // this library. (If something is discovered though, we'll have to make
+        // a breaking change and use an old compat version for decrypting, which
+        // would be extremely inconvenient)
+
+        let key_bytes =
+            hex::decode("eaec3fecf6d988cd8a51bbfba5d5a310d1887459f8433fa0a17fc09f34ee77a4")
+                .unwrap();
+        let key =
+            super::PrivateKey::from_eddsa_pkey_bytes(key_bytes[0..32].try_into().unwrap()).unwrap();
+
+        let plaintext = b"Look at this very important data, I hope nothing happens to it.";
+        let mut ciphertext = [
+            92, 192, 226, 249, 116, 180, 153, 152, 175, 76, 239, 137, 146, 108, 44, 37, 235, 153,
+            93, 214, 162, 184, 139, 81, 116, 9, 222, 126, 28, 56, 67, 115, 240, 125, 224, 21, 125,
+            74, 130, 14, 122, 74, 148, 102, 17, 64, 196, 16, 37, 118, 97, 118, 106, 77, 244, 187,
+            167, 116, 82, 230, 180, 88, 165, 33, 235, 106, 231, 254, 195, 15, 223, 88, 182, 9, 47,
+            197, 182, 25, 47, 55, 238, 79, 132, 141, 236, 27, 95, 59, 91, 226, 208, 222, 159, 8,
+            53, 105, 82, 6, 253, 206, 83, 2, 191, 72, 8, 194, 112, 92, 18, 54, 131, 20, 34, 228,
+            195, 79, 169, 25, 246, 176, 181, 143, 5, 172, 230, 96, 53,
+        ];
+        let message = super::EncryptedMessage::from_bytes(&mut ciphertext).unwrap();
+        let decrypted = key.decrypt_in_place(message).unwrap();
+        assert_eq!(plaintext, decrypted);
     }
 }
